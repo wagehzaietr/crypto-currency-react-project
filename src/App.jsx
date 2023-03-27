353b734c6a59ee298ee0ec12c6b4ea7ec7baa04e
wajih zaietr <107758708+wajihzaietr@users.noreply.github.com>
2023-03-27 05:26:10 +0400

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css'
 import CoinsApi from './Components/Coins/CoinsApi'
-import { Container } from "react-bootstrap";
+import { Container, Nav } from "react-bootstrap";
 
 const App = () => {
   return (
